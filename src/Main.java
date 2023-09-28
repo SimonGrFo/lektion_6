ed32923ded7254087e54d2ccc0c2d4ab0edfed61
simgr <simgrafor03@gmail.com>
2023-09-28 10:41:00 +0200

    0.1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
+
+        for (int i = 0; i < 5; i++) {
+            System.out.println("index: " + i);
+        }
+
+        int number = 5;
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("crash the program");
+        scanner.nextLine();
+
+        System.out.println(number/0);
+
+        try {
+
+        } catch () {
+
+        }
 }
+
